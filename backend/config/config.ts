68d2442b1474faa39e47e969b3d9eb895582cbc9
Bhupendra <bpmaurya24th@gmail.com>
2025-02-24 20:03:25 +0530

    add jwt

--- backend/config/config.ts
+++ backend/config/config.ts
@@ -6,11 +6,14 @@ export const config = Object.freeze({
   db: {
     name: process.env.DB_NAME || "job_portal",
     user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
+    password: process.env.DB_PASSWORD || "bhuppi",
     host: process.env.DB_HOST || "localhost" || "127.0.0.1",
     dialect: "mysql",
   },
   server: {
     port: process.env.PORT || 5000,
   },
+  JWT_SECRET:{
+    JWT_SECRET:process.env.JWT_SECRET as string
+  }
 });
